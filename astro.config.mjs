e7262c8024f6721759bc9efd2a8d2badc60386de
Guillaume Guimond <guillaume.guimond@orange.com>
2023-09-13 11:26:36 +0200

    playing with ui FW components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,5 @@ export default defineConfig({
     adapter: nodejs({
       mode: 'standalone'
     }),
+    integrations: [react()],
 });
